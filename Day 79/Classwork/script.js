0df2f3410041b27657f3e36f811fdab90ccd0bad
Lukaprime12 <lukamtchedlidze@gmail.com>
2024-12-20 14:53:15 +0400

    commit

---
+++ Day 79/Classwork/script.js
@@ -0,0 +1,19 @@
+// alert-ის საშუალებით ინფრომაცია
+alert('Hello This Is alert , I am Luka Mtchedlidze 13 y/o');
+alert('I love color black and white , I love lamborghini');
+
+// alert-ის საშუალებით ავსნათ JS
+alert('So basically JS is like better version of python , its a brain of the site , we can use it for sales , for online payments and many more');
+
+// alert-ის საშუალებით 5 - 5 მაგალითი მიმატება გამოკლებაზე
+alert(10 + 20);
+alert(20 + 30);
+alert(40 + 50);
+alert(60 + 70);
+alert(90 + 100);
+
+alert(10 - 20);
+alert(20 - 30);
+alert(40 - 50);
+alert(60 - 70);
+alert(90 - 100);
---
+++ Day 79/Homework/script.js
@@ -0,0 +1,105 @@
+console.log(1+10);
+console.log(1+100);
+console.log(1+10000);
+console.log(1+176657);
+console.log(1+1657546754);
+console.log(1+17);
+console.log(1+175475);
+console.log(1+17546754);
+console.log(1+174575467);
+console.log(1-15235234);
+console.log(1-15235234);
+console.log(1-15235234);
+console.log(1-15235234);
+console.log(1-15235234);
+console.log(1-15235234);
+console.log(11-5235234);
+console.log(1-15235234);
+console.log(1-+15235234);
+console.log(1-15235234);
+console.log(1-15235234);
+console.log(1/15235234);
+console.log(1/15235234);
+console.log(1/15235234);
+console.log(1/15235234);
+console.log(1/15235234);
+console.log(1/15235234);
+console.log(1/15235234);
+console.log(1/15235234);
+console.log(11/5235234);
+console.log(1/15235234);
+console.log(1*15235234);
+console.log(1*15235234);
+console.log(1*15235234);
+console.log(1*15235234);
+console.log(1*15235234);
+console.log(1*15235234);
+console.log(1*15235234);
+console.log(1*15235234);
+console.log(1*15235234);
+console.log(1*15235234);
+console.log(1*15235234);
+alert(1+1);
+alert(1+176575);
+alert(1+17546754);
+alert(1+1745754);
+alert(1+175675467);
+alert(1+1765475647);
+alert(1+123421);
+alert(1+1789067);
+alert(1+13253);
+alert(1+1125341);
+alert(1-1125341);
+alert(1-1125341);
+alert(1-1125341);
+alert(1-1125341);
+alert(1-1125341);
+alert(1-1125341);
+alert(1-1125341);
+alert(1-1125341);
+alert(1-1125341);
+alert(1-1125341);
+alert(1*1125341);
+alert(1*1125341);
+alert(1*1125341);
+alert(1*1125341);
+alert(1*1125341);
+alert(1*1125341);
+alert(1*1125341);
+alert(1*1125341);
+alert(1*1125341);
+alert(1*1125341);
+alert(1/1125341);
+alert(1/1125341);
+alert(1/1125341);
+alert(1/1125341);
+alert(1/1125341);
+alert(1/1125341);
+alert(1/1125341);
+alert(1/1125341);
+alert(1/1125341);
+alert(1/1125341);
+alert('The world will be destroyed in 10 seconds')
+alert('The world will be destroyed in 9 seconds')
+alert('The world will be destroyed in 8 seconds')
+alert('The world will be destroyed in 7 seconds')
+alert('The world will be destroyed in 6 seconds')
+alert('The world will be destroyed in 5 seconds')
+alert('The world will be destroyed in 4 seconds')
+alert('The world will be destroyed in 3 seconds')
+alert('The world will be destroyed in 2 seconds')
+alert('The world will be destroyed in 1 seconds')
+alert('The world is destroyed')
+console.log('The world will be destroyed in 10 seconds')
+console.log('The world will be destroyed in 9 seconds')
+console.log('The world will be destroyed in 8 seconds')
+console.log('The world will be destroyed in 7 seconds')
+console.log('The world will be destroyed in 6 seconds')
+console.log('The world will be destroyed in 5 seconds')
+console.log('The world will be destroyed in 4 seconds')
+console.log('The world will be destroyed in 3 seconds')
+console.log('The world will be destroyed in 2 seconds')
+console.log('The world will be destroyed in 1 seconds')
+console.log('The world is destroyed')
+alert('HAVE U EVER WONDERED WHAT ALERT IS IN JS??? WELL YOU CAN SEE IT RIGHT NOW , WHATEVER U ARE READING IS AN ALERT! THANK ME LATER!')
+console.log('HAVE U EVER WONDERED WHAT console.log() IS??? THE JS CONSOLE IS A DEVELOPER TOOL FOR DEBUGGING, TESTING, AND INTERACTING WITH WEB APPLICATIONS! THANK ME LATER!')
