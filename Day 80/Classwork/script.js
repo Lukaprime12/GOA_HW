c70d7ca487da759eb4ba6806127a5c58cbecb644
Lukaprime12 <lukamtchedlidze@gmail.com>
2024-12-21 23:23:06 +0400

    commit

---
+++ Day 80/Classwork/script.js
@@ -0,0 +1,14 @@
+let name = 'Luka';
+let lastNane = 'Mtchedlo';
+let age = 13;
+console.log(name , lastNane , age);
+alert(name);
+alert(lastNane);
+alert(age);
+
+let num1 = 10;
+let num2 = 5;
+console.log("This mulitplication is :" , num1 * num2);
+console.log('This sum is: ' , num1 + num2);
+console.log('This division is: ' , num1 / num2);
+console.log('This minusification is: ' , num1 - num2)
---
+++ Day 80/Homework/script.js
@@ -0,0 +1,13 @@
+let name = 'Luka';
+let lastName = 'Mtchedlo';
+let age = 13;
+const name2 = 'Luka';
+const lastName2 = 'Mtchedlo';
+const age2 = 13;
+
+
+
+// const არის კონსტანტას (ისეთი ცვლადის რომელიც არ იცვლება) შესაქმნელი keyword JS-ში , განსხვავებით let-ისა , const მნიშვნელობის შეცვლა გამოიწვევს უსაშველო და ვაი დედა error-ს.
+// let არის ცვლადის შესაქმნელი keyword JS-ში , მაგალითი: let name = 'luka';
+                                                        //    name = 'mate';
+                                                        //    console.log(name) . აქ როგორც ვხედავთ არის შექმნილი თავიდან ცვლადი , მნიშვნელობით luka , შემდეგ ამ ცვლადის მნიშვნელობას ვცვლით mate-ედ , const-ის შემთხვევაში რომ გვექნა იგივე უსაშველო და ვაი დედა error-ს წარმოქმნიდა.
