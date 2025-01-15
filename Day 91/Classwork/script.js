39f72dae96908d00363e54a82a18cb63d548e320
Lukaprime12 <lukamtchedlidze@gmail.com>
2025-01-15 21:56:22 +0400

    commit

---
+++ Day 91/Classwork/script.js
@@ -0,0 +1,21 @@
+// let num = 1;
+// for (let i = 1 ; i <= 1000000000 ; i++) {
+//   result *= i;
+//   if (result > 1000000000) {
+//     break;
+//   }
+// }
+
+
+
+// function greet(name) {
+//     return `welcome , ${name}`
+// }
+
+// console.log(greet("Luka"))
+
+
+let length = prompt("Enter foot: ");
+function feet(toinch) {
+    return length * 12;
+}
